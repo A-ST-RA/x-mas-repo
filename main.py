75d5f17b9e358c6bea6c233353dc8eab55815a88
Vladislav Bachmackiy <astrax200469@gmail.com>
2021-12-31 18:03:05 +0300

    fix: refactored by pep8

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ from time import sleep
 from random import randint
 
 import os
- 
+
+
 def clearConsole():
     os.system('cls')
 
+
 def clearConsole():
     print("\033c\033[3J")
+
 
 def treePage1():
     print(Fore.RED + '     X')
     print(Fore.GREEN + '     ^')
     print(Fore.GREEN + '    ^' + Fore.YELLOW + 'O' + Fore.GREEN + '^')
     print(Fore.GREEN + '   ^^^^^')
-    print(Fore.GREEN + '  ^' + Fore.RED + '0' + Fore.GREEN + '^^' + Fore.BLUE + 'o' + Fore.GREEN + '^^')
+    print(Fore.GREEN + '  ^' + Fore.RED + '0' + Fore.GREEN +
+          '^^' + Fore.BLUE + 'o' + Fore.GREEN + '^^')
     print(Fore.GREEN + '    ^^^')
     print(Fore.GREEN + '   ^^^^^')
-    print(Fore.GREEN + '  ^^' + Fore.CYAN + 'O' + Fore.GREEN +  '^^' + Fore.RED + 'o' + Fore.GREEN + '^')
+    print(Fore.GREEN + '  ^^' + Fore.CYAN + 'O' + Fore.GREEN + '^^' +
+          Fore.RED + 'o' + Fore.GREEN + '^')
     print(Fore.GREEN + ' ^^^^^^^^^')
     print(Fore.MAGENTA + '    |||')
     print(Fore.WHITE + 'Merry X-Mas from A-ST-RA :3')
@@ -32,10 +37,12 @@ def treePage2():
     print(Fore.GREEN + '     ^')
     print(Fore.GREEN + '    ^' + Fore.RED + 'O' + Fore.GREEN + '^')
     print(Fore.GREEN + '   ^^^^^')
-    print(Fore.GREEN + '  ^' + Fore.YELLOW + '0' + Fore.GREEN + '^^' + Fore.CYAN + 'o' + Fore.GREEN + '^^')
+    print(Fore.GREEN + '  ^' + Fore.YELLOW + '0' + Fore.GREEN +
+          '^^' + Fore.CYAN + 'o' + Fore.GREEN + '^^')
     print(Fore.GREEN + '    ^^^')
     print(Fore.GREEN + '   ^^^^^')
-    print(Fore.GREEN + '  ^^' + Fore.WHITE + 'O' + Fore.GREEN +  '^^' + Fore.MAGENTA + 'o' + Fore.GREEN + '^')
+    print(Fore.GREEN + '  ^^' + Fore.WHITE + 'O' + Fore.GREEN +
+          '^^' + Fore.MAGENTA + 'o' + Fore.GREEN + '^')
     print(Fore.GREEN + ' ^^^^^^^^^')
     print(Fore.MAGENTA + '    |||')
     print(Fore.WHITE + 'Merry X-Mas from A-ST-RA :3')
@@ -46,10 +53,12 @@ def treePage3():
     print(Fore.GREEN + '     ^')
     print(Fore.GREEN + '    ^' + Fore.BLACK + 'O' + Fore.GREEN + '^')
     print(Fore.GREEN + '   ^^^^^')
-    print(Fore.GREEN + '  ^' + Fore.RED + '0' + Fore.GREEN + '^^' + Fore.WHITE + 'o' + Fore.GREEN + '^^')
+    print(Fore.GREEN + '  ^' + Fore.RED + '0' + Fore.GREEN +
+          '^^' + Fore.WHITE + 'o' + Fore.GREEN + '^^')
     print(Fore.GREEN + '    ^^^')
     print(Fore.GREEN + '   ^^^^^')
-    print(Fore.GREEN + '  ^^' + Fore.CYAN + 'O' + Fore.GREEN +  '^^' + Fore.BLACK + 'o' + Fore.GREEN + '^')
+    print(Fore.GREEN + '  ^^' + Fore.CYAN + 'O' + Fore.GREEN +
+          '^^' + Fore.BLACK + 'o' + Fore.GREEN + '^')
     print(Fore.GREEN + ' ^^^^^^^^^')
     print(Fore.MAGENTA + '    |||')
     print(Fore.WHITE + 'Merry X-Mas from A-ST-RA :3')
